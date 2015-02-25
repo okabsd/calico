aa1426e46a4c49105808a34ed35aed66dbdb967a
Colin Hall-Coates <rewindhoodie@gmail.com>
2015-02-25 14:04:29 +0000

    Bug fixes, new commands

--- calico.js
+++ calico.js
@@ -3,7 +3,8 @@
   function Calico() {
     piepan.Audio.SetVolume(0.8);
   }
-
+  
+  // Directory and format
   Calico.prototype.audioPrefix = 'audio/';
   Calico.prototype.audioSuffix = '.mp3';
 
@@ -16,6 +17,7 @@
     'hello': 'hello',
     'ready?': 'ready',
     'champ?': 'whois',
+    'my life': 'mylife',
     'collect': 'collect',
     'watching': 'watching',
     'excuse me': 'excuseme',
@@ -37,28 +39,27 @@
   
   // True Commands
   Calico.prototype.commands = {
-    'stop': function (self, args) {
+    // Audio commands
+    'stop': function (self, event, args) {
       piepan.Audio.Stop();
     },
-    'leave now': function (self, args) {
-      console.log('Calico was asked to leave.');
-      piepan.Disconnect();
-    },
-    'help': function (self, args) {
-      piepan.Self.Channel.Send('<b>do [COMMAND]</b> - Call literal commands. e.g., <i>do show +play</i>', false);
-      piepan.Self.Channel.Send('<b>say [COMMAND]</b> - Print associated text. e.g., <i>say cena</i>', false);
-      piepan.Self.Channel.Send('<b>play [COMMAND]</b> - Play associated audio clip. e.g., <i>play slam</i>', false);
-    },
-    'set volume': function (self, args) {
-      var val = Math.max(0, Math.min(parseFloat(args[0], 10), 10)),
+    'set volume': function (self, event, args) {
+      var val = Math.max(0, Math.min(parseFloat(args[0], 10), 1)),
           response = 'Volume: ' + Math.floor(val * 100) + '%';
       
       piepan.Audio.SetVolume(val);
       piepan.Self.Channel.Send(response, false);
     },
-    'show': function (self, args) {
+    
+    // Excess Chat
+    'help': function (self, event, args) {
+      piepan.Self.Channel.Send('<b>do [COMMAND]</b> - Call literal commands. e.g., <i>do show +play</i>', false);
+      piepan.Self.Channel.Send('<b>say [COMMAND]</b> - Print associated text. e.g., <i>say cena</i>', false);
+      piepan.Self.Channel.Send('<b>play [COMMAND]</b> - Play associated audio clip. e.g., <i>play slam</i>', false);
+    },
+    'show': function (self, event, args) {
       var query = self.methodToHash[args[0]],
-          response = 'Commands in [' + query.toUpperCase() + ']: ';
+          response = 'Commands in [' + args[0].toUpperCase() + ']: ';
       
       if (typeof self[query]  === 'object') {
         for (var prop in self[query]) {
@@ -67,21 +68,48 @@
         
         piepan.Self.Channel.Send(response, false);
       }
-    }
+    },
+    'echo': function (self, event, args) {
+      var message = args[0];
+      
+      if (message) {
+        if (event.Sender === null) {
+          piepan.Self.Channel.Send(message, false);
+        } else {
+          event.Sender.Send(message);
+        }
+      }
+    },
+    
+    // Movement
+    'move here': function (self, event, args) {
+      piepan.Self.Move(event.Sender.Channel);
+    },
+    'get out': function (self, event, args) {
+      piepan.Self.Move(piepan.Channels['27']);
+    },
+    'leave now': function (self, event, args) {
+      console.log('Calico was asked to leave.');
+      piepan.Disconnect();
+    },
   };
   
+  // Method and Command relation
   Calico.prototype.methodToHash = {
     'play': 'clips',
     'say': 'responses',
     'do': 'commands'
   };
   
+  // Event Handlers
   Calico.prototype.connected = function (event) {
     console.log('Calico is purring.');
   };
   
   Calico.prototype.roomSwitch = function (event) {
-    event.Channel.Send('Calico.js bot incoming! Type <b>help</b> for a list of basic commands, and <b>do show +[COMMAND]</b> for advanced commands.', false);
+    if (event.Channel.ID !== 27) {
+      event.Channel.Send('Calico.js bot incoming! Type <b>help</b> for a list of basic commands, and <b>do show +[COMMAND]</b> for advanced commands.', false);
+    }
   };
   
   Calico.prototype.talkBack = function (request) {
@@ -108,11 +136,12 @@
     });
   };
   
-  Calico.prototype.issueCommand = function (command) {
+  Calico.prototype.issueCommand = function (event, command) {
     var args = command.split(' +');
         cmd = args.shift();
+        
     if (this.commands[cmd]) {
-      this.commands[cmd](this, args);
+      this.commands[cmd](this, event, args);
     }
   };
 
@@ -123,12 +152,13 @@
 
     var message = event.Message.toLowerCase();
     message = message.trim();
-    message = message.replace(/\s+/g, ' ');
+    message = message.replace(/\s+/g, ' '),
+    resolved = false;
     
     
     // Three main commands
     if (message.substring(0, 2) === 'do') {
-      this.issueCommand(message.slice(3));
+      this.issueCommand(event, message.slice(3));
     }
     
     if (message.substring(0, 4) === 'play') {
@@ -146,6 +176,7 @@
     }
   };
 
+  // Main Object
   var cali = new Calico();
   
   // Events
